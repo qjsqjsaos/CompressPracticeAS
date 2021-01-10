22d0ece9e7ad1ab44801d66ccd042568b2f44905
Beaksooyeol <merrygoaround0726@gmail.com>
2021-01-10 22:20:45 +0900

    ViewPager2

---
+++ app/src/main/java/org/techtown/pager/Fragment1.java
@@ -0,0 +1,22 @@
+package org.techtown.pager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_fragment1, container, false);
+        return rootView;
+    }
+}
